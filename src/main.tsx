d12bb4a0c71e4b6e1d997f0ced333b4bd0a32f42
PlatonKynin <kininplaton@gmail.com>
2025-03-23 21:44:51 +0300

    04Спринт4неделя - 6ая задача

--- src/main.tsx
+++ src/main.tsx
@@ -1,76 +1,39 @@
-import { CSSProperties } from "react"
+import { useEffect } from "react"
 import { createRoot } from "react-dom/client"
-import { SubmitHandler, useForm } from "react-hook-form"
-
-const error: CSSProperties = {
-  color: "red",
-  fontWeight: "bold",
-}
-
-type Inputs = {
-  firstName: string
-  email: string
-}
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from "react-router"
 
 const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>({
-    defaultValues: { firstName: "", email: "" },
-  })
+  const navigate = useNavigate()
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    alert(JSON.stringify(data, null, 2))
-  }
+  useEffect(() => {
+    navigate("/secret/JIUzI1NiIsInR5cCI6IkpXVCJ9")
+  }, [])
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <input {...register("firstName")} placeholder={"Введите имя"} />
-      </div>
-      <div>
-        <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-              message: "Incorrect email",
-            },
-          })}
-          placeholder={"Введите email"}
-        />
-        {/*<input*/}
-        {/*  type="password"*/}
-        {/*  label="Password"*/}
-        {/*  placeholder={"Введите password"}*/}
-        {/*  {...register("password", {*/}
-        {/*    required: "Password is required",*/}
-        {/*    // pattern: {*/}
-        {/*    //    value: /^.{3,}$/,*/}
-        {/*    minLength: {*/}
-        {/*      // ✅ Использование minLength здесь лучше, чем регулярное выражение, потому что оно встроено в react-hook-form и сразу работает с errors*/}
-        {/*      value: 3,*/}
-        {/*      message: "Password must be at least 3 characters long",*/}
-        {/*    },*/}
-        {/*  })}*/}
-        {/*/>*/}
-        {errors.email && <div style={error}>{errors.email.message}</div>}
-      </div>
-      <button type="submit">Отправить</button>
-    </form>
-  )
+  return <div>Login</div>
 }
 
-createRoot(document.getElementById("root")!).render(<Login />)
+const SecretToken = () => {
+  // const token = "no token" // ❗ FIX
+  const { token } = useParams<{ token: string }>() // ✅
+  // const token = useParams().token || "no token"
 
-// 📜 Описание:
-// Загрузив приложение вы увидите ошибку под полем email, но вы еще ничего не ввели.
-// Исправьте строку кода которая выводит сообщение удовлетворяющее условиям:
-// 1) Сообщение об ошибке показывалось только в том случае, когда email введен некорректно.
-// 2) Вместо ERROR должен быть конкретный текст ошибки прописанный в валидации к этому полю.
-// Исправленную версию строки напишите в качестве ответа.
 
-// 🖥 Пример ответа: {<div style={error}>Incorrect email</div>}
-// {errors.email && <div style={error}>ERROR</div>} ✅
+  return <h1>🦾 token: {token}</h1>
+}
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<Login />} />
+      <Route path={"/secret/:token"} element={<SecretToken />} />
+    </Routes>
+  </BrowserRouter>,
+)
+
+// 📜Описание:
+// Исправьте код на строке с пометкой ❗FIX так, чтобы на странице отобразился токен.
+
+//❗Ответ можно указывать с типизацией и без. Учтено несколько вариантов
+
+// 🖥 Пример ответа: const token = 'JIUzI1NiIsInR5cCI6IkpXVCJ9'
+// const { token } = useParams<{ token: string }>() // ✅
